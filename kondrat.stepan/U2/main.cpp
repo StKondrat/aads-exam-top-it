969353aef61f4260df0cc26f1deac33d7b3b3dac
StKondrat <kndrt13@icloud.com>
2026-06-25 12:34:28 +0300

    add structs

--- kondrat.stepan/U2/main.cpp
+++ kondrat.stepan/U2/main.cpp
@@ -1,2 +1,42 @@
+#include <cstddef>
+#include <string>
+
+namespace kondrat
+{
+  struct Person
+  {
+    size_t id;
+    std::string info;
+    bool described;
+  };
+
+  struct Meeting
+  {
+    size_t first;
+    size_t second;
+    size_t time;
+  };
+
+  struct PersonStorage
+  {
+    Person * data;
+    size_t size;
+    size_t capacity;
+  };
+
+  struct MeetingStorage
+  {
+    Meeting * data;
+    size_t size;
+    size_t capacity;
+  };
+
+  struct ProgramArgs
+  {
+    const char * input;
+    const char * data;
+  };
+}
+
 int main()
 {}
